1c35d63b5d5cb82ed8d7ecb4d09966dd2660216e
PavelKuropatin <pavelkuropatin@gmail.com>
2016-11-29 03:13:44 +0300

    exe not open images

--- controller/Manager.py
+++ controller/Manager.py
@@ -3,12 +3,11 @@ from pygame import Surface
 from model.logic.LogicGame import LogicGame
 from view.ViewBackgound import ViewBackground
 
-window_width = 800
-window_height = 640
-background_color = (92, 146, 252)
-
 
 def main():
+    window_width = 800
+    window_height = 640
+    background_color = (92, 146, 252)
     pygame.init()
     window = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Mario")
